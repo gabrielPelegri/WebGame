3ab60724d96470e041d5e3ee9876907723611211
Gabriel Pelegri <gpelerii@myuax.com>
2022-10-24 11:00:21 +0200

    Creada carpeta statusModels Clase StatusModels ahora es abstracta Implementado constructor y métodos

---
+++ src/main/java/com/example/test/models/statusModels/StatusModel.java
@@ -0,0 +1,23 @@
+package com.example.test.models.statusModels;
+
+import com.example.test.models.CardModel;
+
+import javax.smartcardio.Card;
+
+public abstract class StatusModel {
+
+    CardModel cardModel;
+
+    public StatusModel(CardModel cardModel) {
+        this.cardModel = cardModel;
+    }
+
+    public CardModel getCardModel() {
+        return cardModel;
+    }
+
+    public void setCardModel(CardModel cardModel) {
+        this.cardModel = cardModel;
+    }
+
+}
