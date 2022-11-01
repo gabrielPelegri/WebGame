11b456124e103a5d24b75ac8155ccfa1ef699689
Gabriel Pelegri <gpelerii@myuax.com>
2022-11-01 13:05:32 +0100

    Array cambiado a ArrayList para tener varios status

--- src/main/java/com/example/test/models/CardModel.java
+++ src/main/java/com/example/test/models/CardModel.java
@@ -1,11 +1,13 @@
 package com.example.test.models;
 
+import java.util.ArrayList;
+
 public class CardModel {
 
     private String name;
     private Long health;
     private Long damage;
-    private String status;
+    private ArrayList<CardModel> status;
     private Long life;
     private CardModel cardModelObjetivo;
 
@@ -33,11 +35,11 @@ public class CardModel {
         this.damage = damage;
     }
 
-    public String getStatus() {
+    public ArrayList<CardModel> getStatus() {
         return status;
     }
 
-    public void setStatus(String status) {
+    public void setStatus(ArrayList<CardModel> status) {
         this.status = status;
     }
 
@@ -57,16 +59,16 @@ public class CardModel {
         this.cardModelObjetivo = cardModelObjetivo;
     }
 
-    public String victory(CardModel c){
-        if(this.life > c.life){
-            return String.format("Soy el ganador de nombre %s",this.name);
+    public String victory(CardModel c) {
+        if (this.life > c.life) {
+            return String.format("Soy el ganador de nombre %s", this.name);
         }
-        return String.format("Soy el ganador de nombre %s",c.name);
+        return String.format("Soy el ganador de nombre %s", c.name);
     }
 
-    public void combat(CardModel c){
+    public void combat(CardModel c) {
         long resta = this.life - c.damage;
         System.out.println(
-                String.format(("La carta %s ha hecho %d a la carta %s"), this.name,resta,c.name));
+                String.format(("La carta %s ha hecho %d a la carta %s"), this.name, resta, c.name));
     }
 }
