e7a1f6ca5d30977718e87606d57d2f6ea6213040
Gabriel Pelegri <gpelerii@myuax.com>
2022-11-13 16:35:04 +0100

    Cambios en login Paquete de datos

--- src/main/java/com/example/test/controllers/LoginController.java
+++ src/main/java/com/example/test/controllers/LoginController.java
@@ -3,12 +3,18 @@ package com.example.test.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
     public String login(Model model) {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginPost(Model model) {
         return "login";
     }
 }
---
+++ src/main/java/com/example/test/data/UserData.java
@@ -0,0 +1,64 @@
+package com.example.test.data;
+
+import javax.persistence.*;
+
+@Entity
+public class UserData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "mail", nullable = false)
+    private String mail;
+
+    @Column(name = "age", nullable = false)
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
