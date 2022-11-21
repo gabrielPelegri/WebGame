c1a25e3efd36eb7d8c9499d58b0ddc867dc81c0b
Gabriel Pelegri <gpelerii@myuax.com>
2022-11-21 10:46:17 +0100

    Cambiado a CrudRepository y anotaciones de MongoDB

--- src/main/java/com/example/test/data/UserData.java
+++ src/main/java/com/example/test/data/UserData.java
@@ -1,77 +1,54 @@
 package com.example.test.data;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import javax.persistence.*;
 
-@Entity(name = "users")
+@Document("users")
 public class UserData {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
     private Long id;
-
-    @Column(name = "name", nullable = false)
     private String name;
-
-    @Column(name = "password", nullable = false)
     private String password;
-
-    @Column(name = "mail", nullable = false)
     private String mail;
-
-    @Column(name = "age", nullable = false)
     private Integer age;
-
-    @Column(name = "wins")
     private Integer wins;
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public String getMail() {
         return mail;
     }
-
     public void setMail(String mail) {
         this.mail = mail;
     }
-
     public Integer getAge() {
         return age;
     }
-
     public void setAge(Integer age) {
         this.age = age;
     }
-
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public Integer getWins() {
         return wins;
     }
-
     public void setWins(Integer wins) {
         this.wins = wins;
     }
-
     public UserData() {
     }
 
--- src/main/java/com/example/test/models/UserRepository.java
+++ src/main/java/com/example/test/models/UserRepository.java
@@ -1,14 +1,14 @@
 package com.example.test.models;
 
 import com.example.test.data.UserData;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserData, Long> {
+public interface UserRepository extends CrudRepository<UserData, Long> {
     Optional<UserData> findByMail(String mail);
 
     Optional<UserData> findByName(String name);
